d98ff92db4d374de89ea53315f15e2fbb2bb8f7b
ContinentalBreakfast17 <hunterrhollant@gmail.com>
2019-04-23 19:21:26 -0400

    Initial commit

---
+++ src/components/auth.vue
@@ -0,0 +1,52 @@
+<template>
+	<div class="auth">
+		<span style="display: block;">Username: <input type="text" v-model="username" @keyup.enter="login"/></span>
+		<span style="display: block;">Password: <input type="password" v-model="password" @keyup.enter="login"/></span>
+		<button @click="login()">Sign In</button>
+	</div>
+</template>
+
+<script>
+import { Auth } from 'aws-amplify'
+import { components, AmplifyEventBus } from 'aws-amplify-vue';
+import VueCookies from 'vue-cookies'
+
+export default {
+	name: 'auth',
+	components: {
+		...components,
+		AmplifyEventBus
+	},
+	data: function(){
+		return {
+			username: '',
+			password: ''
+		}
+	},
+	methods: {
+		login: function(){
+			Auth.signIn({
+				'username': this.username,
+				'password': this.password
+			}).then(user => {
+				// handle MFA and new password required https://aws-amplify.github.io/docs/js/authentication#common-authentication-use-cases
+				console.log(user)
+				this.getToken()
+			}).catch(err => {
+				console.log(err)
+			});
+		},
+		getToken: function(){
+			Auth.currentSession()
+			.then(data => {
+				console.log(data)
+			}).catch(err => {
+				console.log(err)
+			});
+		}
+	},
+	mounted: function(){
+
+	}
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,60 @@
+import Vue from 'vue'
+import App from './app.vue'
+import router from './router'
+
+import Amplify, * as AmplifyModules from 'aws-amplify'
+import { AmplifyPlugin } from 'aws-amplify-vue'
+
+Vue.use(AmplifyPlugin, AmplifyModules)
+
+Vue.config.productionTip = false
+
+Amplify.configure({
+	Auth: {
+
+		// REQUIRED only for Federated Authentication - Amazon Cognito Identity Pool ID
+		//identityPoolId: 'XX-XXXX-X:XXXXXXXX-XXXX-1234-abcd-1234567890ab',
+		
+		// REQUIRED - Amazon Cognito Region
+		region: 'us-east-2',
+
+		// OPTIONAL - Amazon Cognito Federated Identity Pool Region 
+		// Required only if it's different from Amazon Cognito Region
+		//identityPoolRegion: 'XX-XXXX-X',
+
+		// OPTIONAL - Amazon Cognito User Pool ID
+		userPoolId: 'us-east-2_uJ1DWumvk',
+
+		// OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
+		userPoolWebClientId: '59o91ktt6k5evqtkrpbvf1es95',
+
+		// OPTIONAL - Enforce user authentication prior to accessing AWS resources or not
+		mandatorySignIn: true,
+
+		// // OPTIONAL - Configuration for cookie storage
+		// // Note: if the secure flag is set to true, then the cookie transmission requires a secure protocol
+		// cookieStorage: {
+		// // REQUIRED - Cookie domain (only required if cookieStorage is provided)
+		// 	domain: '.yeezyfan.club',
+		// // OPTIONAL - Cookie path
+		// 	path: '/',
+		// // OPTIONAL - Cookie expiration in days
+		// 	expires: 365,
+		// // OPTIONAL - Cookie secure flag
+		// // Either true or false, indicating if the cookie transmission requires a secure protocol (https).
+		// 	secure: true
+		// },
+
+		// // OPTIONAL - customized storage object
+		// storage: new MyStorage(),
+		
+		// OPTIONAL - Manually set the authentication flow type. Default is 'USER_SRP_AUTH'
+		//authenticationFlowType: 'USER_PASSWORD_AUTH'
+	}
+});
+
+
+new Vue({
+	router,
+	render: h => h(App)
+}).$mount('#app')
